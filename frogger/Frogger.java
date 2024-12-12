829d67becfeee6c17116909793699ab0a39192dc
Raihan Andi Saungnaga <149381964+sngraihan@users.noreply.github.com>
2024-12-12 17:17:45 +0700

    main

---
+++ frogger/Frogger.java
@@ -0,0 +1,32 @@
+package frogger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author raiha
+ */
+public class Frogger extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        stage.setTitle("Frogger");
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
