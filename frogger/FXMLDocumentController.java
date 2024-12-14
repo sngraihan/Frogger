095926f816c16cd510a351dfc1cb956e20bd4e98
Raihan Andi Saungnaga <149381964+sngraihan@users.noreply.github.com>
2024-12-14 09:59:15 +0700

    abstract class

--- frogger/FXMLDocumentController.java
+++ frogger/FXMLDocumentController.java
@@ -27,3 +27,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+abstract class GameElement {
+    protected double x;
+    protected double y;
+
+    public GameElement(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public abstract Node getNode();
+}
