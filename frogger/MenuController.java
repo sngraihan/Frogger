cc6275152b0b5c30c04ff27a69baf063d13a8dfb
GALVIN RAKA <151343022+GALVINRAKA@users.noreply.github.com>
2024-12-14 18:51:21 +0700

    Update MenuController.java

--- frogger/MenuController.java
+++ frogger/MenuController.java
@@ -18,3 +18,41 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author raiha
+ */
+public class MenuController implements Initializable {
+
+    @FXML
+    private ImageView reset;
+    
+    private MediaPlayer clk;
+    private static final Media CLICK = new Media(FXMLDocumentController.class.getResource("/frogger/sfx/click.mp3").toExternalForm());
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        clk = new MediaPlayer(CLICK);
+        clk.setVolume(0.7);
+    }    
+
+    @FXML
+    private void tekanSaya(MouseEvent event) {
+        clk.play();
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            
+            stage.setScene(new Scene(root));
+            stage.setTitle("Game Scene");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
