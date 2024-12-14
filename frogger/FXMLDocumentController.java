8cb1c92b6e7cf140330417baaa1ebc6877fa39ba
Raihan Andi Saungnaga <149381964+sngraihan@users.noreply.github.com>
2024-12-14 09:45:19 +0700

    import

--- frogger/FXMLDocumentController.java
+++ frogger/FXMLDocumentController.java
@@ -1,3 +1,29 @@
-public class FXMLDocumentController {
-    
-}
+package frogger;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.animation.AnimationTimer;
+import javafx.animation.FadeTransition;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
