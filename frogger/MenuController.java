a55cef76ff79316adc2152d0696581f057c043a7
GALVIN RAKA <151343022+GALVINRAKA@users.noreply.github.com>
2024-12-14 18:38:32 +0700

    Update MenuController.java

--- frogger/MenuController.java
+++ frogger/MenuController.java
@@ -1,3 +1,20 @@
-public class MenuController {
-    
-}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package frogger;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
